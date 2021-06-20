8eae4871a3fc4b1c5e9c2c2ba48466a84f570c6b
MidnQP <midnightquantumprogrammer@gmail.com>
2021-06-21 03:02:21 +0600

    Removed xrandr, enter brightnessctl

--- midnqp-init-i3.sh
+++ midnqp-init-i3.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 
-brightness=`xrandr --verbose | grep -m 1 -i brightness | cut -f2 -d ' '`
+blu="\033[34m"
+grn="\033[32m"
+red="\033[31m"
+res="\033[0m"
+input="$blu[input]$res"
+info="$grn[info]$res"
 
 
-if [ "$brightness" = "1.0" ]; then
-	xrandr --output eDP-1 --brightness 0.5
-	xrandr --output eDP-1 --mode 1368x768
+sudo brightnessctl set 400 intel_backlight
+
+# String="String"
+# echo $String | tail -c +2 | head -c -2
+# Output: trin
+# tail removed S, head removed g.
 
 
-	for n in {0..3}
-	do
-		sudo cpufreq-set -g powersave -c $n
-	done
-else
-	echo "init-i3: You sure you wanna run me?"
-fi
+#xrandr --output eDP-1 --brightness 0.15
+#xrandr --output eDP-1 --mode 1368x768
+#xrandr --output eDP-1 --mode 1920x1080 --rate 48.00
+
+for n in {0..3}
+do
+	echo ""
+	sudo cpufreq-set -g powersave -c $n
+done
