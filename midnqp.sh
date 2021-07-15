0b02f18c9ce0415c013f3d23998123d1ea8c5a5c
MidnQP <midnightquantumprogrammer@gmail.com>
2021-07-15 12:07:58 +0600

    midnqp

--- midnqp.sh
+++ midnqp.sh
@@ -7,16 +7,6 @@
 
 
 
-# git wget vim-nox brightnessctl
-red="\033[31m"
-grn="\033[32m"
-res="\033[0m"
-midn=
-err="$red[error]$res"
-
-
-
-
 apt() {
 	sudo apt-get update -y
 	sudo apt-get upgrade -y
@@ -26,6 +16,11 @@ setupGit() {
 	git config --global credential.helper store
 	git config --global user.email "midnightquantumprogrammer@gmail.com"
 	git config --global user.name "Midnqp"
+	git config --global core.fileMode false
+}
+setupVimrc() {
+	wget https://raw.githubusercontent.com/midnqp/midnqp/midnqp/dotfiles/vimrc -O ~/.vimrc
+	mkdir -p ~/.vim/view
 }
 getChrome() {
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
